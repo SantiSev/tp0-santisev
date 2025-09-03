1a4185410c485f7c9f07c07df9b2d6a00e02397a
sev <santisevitz@hotmail.com>
2025-09-03 05:00:57 -0300

    handle 8kb transmission limitation

--- client/common/protocol/bet-handler.go
+++ client/common/protocol/bet-handler.go
@@ -20,6 +20,10 @@ func NewBetHandler() *BetHandler {
 
 func (b *BetHandler) SendBets(bets string, connSock *network.ConnectionInterface) error {
 
+	if len(bets) > MAX_BATCH_SIZE {
+		return fmt.Errorf("bets size too big to send: %d bytes (max %d)", len(bets), MAX_BATCH_SIZE)
+	}
+
 	err := connSock.SendData([]byte(HEADER))
 	if err != nil {
 		return err
