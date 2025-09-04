f126a50ddb4056ae34c15d9d777e0b5499f76bf9
sev <santisevitz@hotmail.com>
2025-09-04 14:33:59 -0300

    small fix

--- generar-compose.sh
+++ generar-compose.sh
@@ -8,7 +8,7 @@ fi
 YAML_FILE="$1"
 AMOUNT_CLIENTS="$2"
 
-cat > docker-compose-dev.yaml << EOF
+cat > "$YAML_FILE" << EOF
 name: tp0
 services:
   server:
@@ -24,7 +24,7 @@ services:
 EOF
 
 for i in $(seq 1 "$AMOUNT_CLIENTS"); do
-    cat >> docker-compose-dev.yaml << EOF
+    cat >> "$YAML_FILE" << EOF
 
   client$i:
     container_name: client$i
@@ -43,7 +43,7 @@ for i in $(seq 1 "$AMOUNT_CLIENTS"); do
 EOF
 done
 
-cat >> docker-compose-dev.yaml << EOF
+cat >> "$YAML_FILE" << EOF
 
 networks:
   testing_net:
