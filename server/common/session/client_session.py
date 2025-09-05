a033099344e273cb69266b79065de975e9ebe7bf
sev <santisevitz@hotmail.com>
2025-09-04 21:29:56 -0300

    haciendo que el while loop del client session no sea while True

--- server/common/session/client_session.py
+++ server/common/session/client_session.py
@@ -17,9 +17,10 @@ class ClientSession:
         self.connection_interface = connection_interface
         self.lottery_service = lottery_service
         self.protocol_handler = AgencyHandler()
+        self.is_running = True
 
     def begin(self) -> bool:
-        while True:
+        while self._is_running():
             try:
                 betBatch, more_bets_remaining = self.protocol_handler.get_bets(
                     self.connection_interface, self.agency_id
@@ -27,10 +28,13 @@ class ClientSession:
 
                 if not more_bets_remaining:
                     logging.info(f"action: all_bets_received | result: success")
-                    break
-
-                self.lottery_service.place_bets(betBatch)
-                self.protocol_handler.confirm_batch(self.connection_interface, True)
+                    self.is_running = False
+                else:
+                    logging.info(
+                        f"action: bet_batch_received | result: success | cantidad: {len(betBatch)}"
+                    )
+                    self.lottery_service.place_bets(betBatch)
+                    self.protocol_handler.confirm_batch(self.connection_interface, True)
 
             except Exception as e:
                 logging.error(
@@ -43,6 +47,9 @@ class ClientSession:
             f"action: apuesta_recibida | result: success | cantidad: {len(agencyBets)}"
         )
         return True
+
+    def _is_running(self) -> bool:
+        return self.is_running
 
     def send_results(self):
         """Tally and log the results of the lottery"""
