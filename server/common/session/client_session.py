487e9e8d155bb36143cfa6af1c27094f47606009
sev <santisevitz@hotmail.com>
2025-09-04 21:14:48 -0300

    update client session to have the begin function to not do while true

--- server/common/session/client_session.py
+++ server/common/session/client_session.py
@@ -17,9 +17,10 @@ class ClientSession:
         self.connection_interface = connection_interface
         self.lottery_service = lottery_service
         self.protocol_handler = AgencyHandler()
+        self.is_running = True
 
     def begin(self):
-        while True:
+        while self._is_running():
             try:
                 betBatch, more_bets_remaining = self.protocol_handler.get_bets(
                     self.connection_interface, self.agency_id
@@ -27,10 +28,11 @@ class ClientSession:
 
                 if not more_bets_remaining:
                     logging.info(f"action: all_bets_received | result: success")
-                    break
-
-                self.lottery_service.place_bets(betBatch)
-                self.protocol_handler.confirm_batch(self.connection_interface, True)
+                    self.is_running = False
+                else:
+                    logging.info(f"action: batch_received | result: success")
+                    self.lottery_service.place_bets(betBatch)
+                    self.protocol_handler.confirm_batch(self.connection_interface, True)
 
             except Exception as e:
                 logging.error(
@@ -43,12 +45,14 @@ class ClientSession:
             f"action: apuesta_recibida | result: success | cantidad: {len(agencyBets)}"
         )
 
+    def _is_running(self) -> bool:
+        return self.is_running
+
     def send_results(self):
         """Tally and log the results of the lottery"""
         winners = self.lottery_service.draw_winners(self.agency_id)
         logging.info(f"action: send_results | result: success | winners: {winners}")
         self.protocol_handler.send_winners(self.connection_interface, winners)
-
 
     def finish(self) -> None:
         self.connection_interface.close()
