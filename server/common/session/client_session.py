1b039f9c26c528c5a4e4189a49e2cab7ff526e7e
sev <santisevitz@hotmail.com>
2025-09-04 21:19:04 -0300

    fix ej7 to match with ej8

--- server/common/session/client_session.py
+++ server/common/session/client_session.py
@@ -17,9 +17,10 @@ class ClientSession:
         self.connection_interface = connection_interface
         self.lottery_service = lottery_service
         self.protocol_handler = AgencyHandler()
+        self.is_running = True
 
-    def begin(self) -> bool:
-        while True:
+    def begin(self):
+        while self._is_running():
             try:
                 betBatch, more_bets_remaining = self.protocol_handler.get_bets(
                     self.connection_interface, self.agency_id
@@ -27,29 +28,31 @@ class ClientSession:
 
                 if not more_bets_remaining:
                     logging.info(f"action: all_bets_received | result: success")
-                    break
-
-                self.lottery_service.place_bets(betBatch)
-                self.protocol_handler.confirm_batch(self.connection_interface, True)
+                    self.is_running = False
+                else:
+                    logging.info(f"action: batch_received | result: success")
+                    self.lottery_service.place_bets(betBatch)
+                    self.protocol_handler.confirm_batch(self.connection_interface, True)
 
             except Exception as e:
                 logging.error(
-                    f"action: apuesta_recibida | result: fail | error: {e}"
+                    f"action: apuesta_recibida | result: fail | cliente se ha desconectado"
                 )
                 self.protocol_handler.confirm_batch(self.connection_interface, False)
-                return False
+                raise e
         agencyBets = self.lottery_service.get_bets_by_agency(self.agency_id)
         logging.info(
             f"action: apuesta_recibida | result: success | cantidad: {len(agencyBets)}"
         )
-        return True
+
+    def _is_running(self) -> bool:
+        return self.is_running
 
     def send_results(self):
         """Tally and log the results of the lottery"""
         winners = self.lottery_service.draw_winners(self.agency_id)
         logging.info(f"action: send_results | result: success | winners: {winners}")
         self.protocol_handler.send_winners(self.connection_interface, winners)
-
 
     def finish(self) -> None:
         self.connection_interface.close()
